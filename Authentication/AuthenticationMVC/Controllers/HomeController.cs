70f8d5e26c2904c8bfbeacb4a515af0af68a577a
renan santos <renanwillian87@gmail.com>
2020-04-19 22:30:33 -0300

    All project

---
+++ Authentication/AuthenticationMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "TemNome")]
+        public async Task<IActionResult> Privacy()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            //return Content(User.FindFirst("Fullname").Value);
+            //return Content(user.Email);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
